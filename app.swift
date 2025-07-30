bcdca76419e974b8939600f4bc186a79039fdcc7
Karam Fahad <5566383+karamfd@users.noreply.github.com>
2025-07-30 20:17:30 -0300

    Separated lowercase and uppercase letters

--- app.swift
+++ app.swift
@@ -9,10 +9,11 @@ while true {
   if passwordLength < 8 || passwordLength > 32 {
     print("Error: password must be between 8 and 32 characters")
   } else {
-      let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      let lowercase = "abcdefghijklmnopqrstuvwxyz"
+      let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
       let digits = "0123456789"
       let punctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
-      let characters = letters + digits + punctuation
+      let characters = lowercase + uppercase + digits + punctuation
       
       let password = String((0..<passwordLength).map { _ in
         characters.randomElement()!
